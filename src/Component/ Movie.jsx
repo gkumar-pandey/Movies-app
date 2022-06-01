3c1918b9c857d8b9b8d65aaaa2887e38e37aa698
gkumar-pandey <gautamshekhar078@gmail.com>
2022-06-01 14:13:15 +0530

    movie list component created

--- src/Component/ Movie.jsx
+++ src/Component/ Movie.jsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-import { search } from "./Movies";
+import axios from "axios";
 
+
+
+ 
 const Movie = () => {
+  const [content, setcontent] = useState([]);
+
+  const TrendingMoviesList = async () => {
+    const {data} = await axios.get(
+      `https://api.themoviedb.org/3/trending/all/day?api_key=5dd3b7469943c761e37c9378b1a51f26`
+    );
+    setcontent(data.results)
+    
+  };
+  useEffect(()=>{
+    TrendingMoviesList();
+  },[])
+  console.log(content)
   return (
     <div>
       <h1 style={style.trending}>Trending</h1>
 
       <div style={style.movielist}>
-      {search.map((item, idx) => {
-        return <MovieCard img={item.Poster} />;
-      })}
+        {content.map((item, idx) => {
+           
+          return <MovieCard img={item.poster_path} title={item.original_title || item.name} key={item.idx} />;
+        })}
       </div>
-      
     </div>
   );
 };
@@ -22,11 +38,13 @@ let style = {
     textAlign: "center",
     padding: "1rem",
   },
-  movielist : {
-    border: '1px solid red',
-    display:'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly'
-  }
+  movielist: {
+    // border: "1px solid red",
+    padding: '0rem 4rem',
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    
+  },
 };
 export default Movie;
--- src/Component/MovieCard.jsx
+++ src/Component/MovieCard.jsx
@@ -1,17 +1,26 @@
 import React from "react";
+import { img_500 } from "./Config";
+import './MovieCardStyle.css'
 
 const MovieCard = (props) => {
   return (
     <div>
-      <div className="card" style={style.cardwidth}>
-        <img src={props.img} className="card-img-top" alt="..." style={style.movieposter}/>
+      <div className="cardwidth" style={style.cardwidth} >
+        <img src={img_500 + props.img} className="movieposter" alt="..." style={style.movieposter} />
+
         <h5 className="card-title" style={style.cardtitle}>
-          Card title
+          {props.title}
         </h5>
 
-        <a href="#" className="btn btn-primary" style={style.cardbtn}>
-          Add to Favourites
-        </a>
+        <div style={style.btnwrapper}>
+          <a href="#" className="btn btn-primary" style={style.cardbtn}>
+            Add to Favourites
+          </a>
+
+        </div>
+
+
+
       </div>
     </div>
   );
@@ -21,31 +30,38 @@ const style = {
   cardwidth: {
     width: "20rem",
     position: "relative",
-    height: '500px',
-    padding: '1rem',
-    border: 'none' 
+    height: 'auto',
+
+    border: 'none',
+    // border: '1px solid red',
+    margin: '1rem 0rem',
+
+
   },
   movieposter: {
     height: '100%',
-    width:'100%',
-    borderRadius: '10px'
-  },
-  cardtitle: {
-    position: "absolute",
-    top: "4rem",
-    color: "white",
-    fontSize: "2rem",
-    left: "5rem",
-  },
-  cardbtn: {
-    position: "absolute",
-    bottom: "30%",
-    left: "25%",
-    width: '10rem',
-    padding: '10px',
-    fontSize: '1rem'
+    width: '100%',
+    borderRadius: '10px',
+
 
   },
+  cardtitle: {
+    position: 'absolute',
+    top: '4rem',
+    color: 'white',
+    //  border: '1px solid white',
+    width: '100%',
+    fontSize: '1.8rem',
+    textAlign: 'center'
+
+  },
+  btnwrapper: {
+    position: 'absolute',
+    bottom: '3rem',
+    width: '100%',
+    textAlign: 'center'
+  }
+
 };
 
 export default MovieCard;
