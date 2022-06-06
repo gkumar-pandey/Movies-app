f3a945ceb2d3e56aac615bd739dee113711742e8
gkumar-pandey <gautamshekhar078@gmail.com>
2022-06-06 08:21:14 +0530

    update navbar css

--- src/Component/MovieCard.jsx
+++ src/Component/MovieCard.jsx
@@ -1,23 +1,34 @@
-import React from "react";
-import { img_original } from "./Config";
+import React, { useState } from "react";
+import { img_500 } from "./Config";
 import './MovieCardStyle.css'
 
 const MovieCard = (props) => {
+  const [mouseEnter, setmouseEnter] = useState(false)
+  const IsMouseEnter = () => {
+    setmouseEnter(true)
+  }
+  const IsMouseLeave = () => {
+    setmouseEnter(false)
+
+  }
   return (
 
-    <div className="cardwidth"  >
-      <img src={img_original + props.img} className="movieposter" alt="..." />
+    <div className="cardwidth" onMouseEnter={IsMouseEnter} onMouseLeave={IsMouseLeave} >
+      <img src={img_500 + props.img} className="movieposter" alt="..." />
 
       <h5 className="cardtitle"  >
         {props.title}
       </h5>
 
-      <div className="btnwrapper">
-        <a href="#" className="btn btn-primary">
-          Add to Favourites
-        </a>
+      {
+        mouseEnter ? <div className="btnwrapper">
+          <a href="#" className="btn btn-primary">
+            Add to Favourites
+          </a>
 
-      </div>
+        </div> : ''
+      }
+
 
 
 
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,33 +1,35 @@
-import React from 'react'
-
+import React from "react";
+import './Navbar.css';
 const Navbar = () => {
   return (
     <div>
-        <nav style={style.navbar}>
-            <h1 style={style.navlogo}>Movies</h1>
-            <h5 style={style.favMoivie}>Favourites</h5>
-        </nav>
+      <nav  className='navbar'>
+        <div className="logowrapper">
+          <h1 className="navlogo">Movies</h1>
+          <h5 className="favMovie">Favourites</h5>
+        </div>
+
+        <form className="d-flex searchbar" role="search"  >
+          <input
+            className="form-control shadow-none me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-light" type="submit">
+            Search
+          </button>
+        </form>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
 const style = {
-    navbar: {
-        height: '6vh',
-        width: '100%',
-        backgroundColor: '#02576C',
-        color: 'white',
-        display: 'flex',
-        alignItems: 'center',
-        padding: '10px'   
-    },
-    navlogo: {
-        fontSize: '1.8rem',
-         
-    },
-    favMoivie:{
-        fontSize: '1.2rem',
-        marginLeft: '1rem'
-    }
-}
+  
+   
+   
+   
+ 
+};
 export default Navbar;
